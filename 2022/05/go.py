16fe68b7063ead4f0743c9dfeed95010d4f93233
Ryszard Chrystyniak <okazet@gmail.com>
2022-12-05 15:29:36 +0100

    day 5 refactor

--- 2022/05/go.py
+++ 2022/05/go.py
@@ -1,64 +1,45 @@
+import copy
+
 def get_stacks(crate_input):
   cols_no = int(crate_input[-1][-2])
   ci = crate_input[:-1]
-
   stacks = []
     
   for i in range(cols_no):
     stacks.append([])
     for c in ci:
       crate_name = c[i*4+1]
-      if crate_name.strip() != '': stacks[i].append(crate_name)
+      if crate_name.strip(): stacks[i].append(crate_name)
 
-  for i in range(cols_no):
-    stacks[i].reverse()
-
-  return stacks
+  return [i[::-1] for i in stacks]
 
 def get_program(program_input):
   program = []
   for line in program_input:
     spl = line.split(' ')
-    program.append({
-      'q': int(spl[1]),
-      'from': int(spl[3]),
-      'to': int(spl[5])
-    })
+    program.append([int(spl[1]), int(spl[3]) - 1, int(spl[5]) - 1])
   return program
 
 def calc_result(stacks_result):
-  result = ''
-  for i in range(len(stacks_result)):
-    result += stacks_result[i][-1:][0]
-  return result
+  return str.join('', [i[-1] for i in stacks_result])
 
 def rearrange(stacks, program, reverse):
-  for i in program:
-    q = i.get('q')
-    o = i.get('from') - 1
-    d = i.get('to') - 1
+  for q, o, d in program:
     take = stacks[o][-q:]
     if reverse: take.reverse()
-    stacks[o] = stacks[o][:-q]
+    del stacks[o][-q:]
     stacks[d].extend(take)
   return stacks
 
 with open('input.txt') as f:
-  lines = f.read().splitlines()
-  empty_index = lines.index('')
-  crate_input = lines[:empty_index]
-  program_input = lines[empty_index + 1:]
+  crate_input, program_input = f.read().split('\n\n')
 
-  program = get_program(program_input)
+  stacks = get_stacks(crate_input.splitlines())
+  program = get_program(program_input.splitlines())
   
-  # part one
-  stacks = get_stacks(crate_input)
-  stacks_result = rearrange(stacks, program, True)
+  stacks_result_one = rearrange(copy.deepcopy(stacks), program, True)
+  stacks_result_two = rearrange(copy.deepcopy(stacks), program, False)
+  
+  print(calc_result(stacks_result_one))
+  print(calc_result(stacks_result_two))  
 
-  print(calc_result(stacks_result))
-
-  # part two
-  stacks = get_stacks(crate_input)
-  stacks_result = rearrange(stacks, program, False)
-
-  print(calc_result(stacks_result))  
