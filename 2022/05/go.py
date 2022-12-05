5935a00c0f10137c7101f3dd803dc24ac0a8dcc6
Ryszard Chrystyniak <okazet@gmail.com>
2022-12-05 09:13:20 +0100

    day 5 little refactor

--- 2022/05/go.py
+++ 2022/05/go.py
@@ -1,13 +1,12 @@
-def get_stacks(cratepart):
-  cols_no = int(cratepart[-1][-2])
-  cr = cratepart[:-1]
+def get_stacks(crate_input):
+  cols_no = int(crate_input[-1][-2])
+  ci = crate_input[:-1]
 
   stacks = []
+    
   for i in range(cols_no):
     stacks.append([])
-
-  for i in range(cols_no):
-    for c in cr:
+    for c in ci:
       crate_name = c[i*4+1]
       if crate_name.strip() != '': stacks[i].append(crate_name)
 
@@ -16,9 +15,9 @@ def get_stacks(cratepart):
 
   return stacks
 
-def get_program(instructionpart):
+def get_program(program_input):
   program = []
-  for line in instructionpart:
+  for line in program_input:
     spl = line.split(' ')
     program.append({
       'q': int(spl[1]),
@@ -27,42 +26,39 @@ def get_program(instructionpart):
     })
   return program
 
+def calc_result(stacks_result):
+  result = ''
+  for i in range(len(stacks_result)):
+    result += stacks_result[i][-1:][0]
+  return result
+
+def rearrange(stacks, program, reverse):
+  for i in program:
+    q = i.get('q')
+    o = i.get('from') - 1
+    d = i.get('to') - 1
+    take = stacks[o][-q:]
+    if reverse: take.reverse()
+    stacks[o] = stacks[o][:-q]
+    stacks[d].extend(take)
+  return stacks
+
 with open('input.txt') as f:
   lines = f.read().splitlines()
-  empty = lines.index('')
-  cratepart = lines[:empty]
-  instructionpart = lines[empty + 1:]
+  empty_index = lines.index('')
+  crate_input = lines[:empty_index]
+  program_input = lines[empty_index + 1:]
 
-  stacks = get_stacks(cratepart)
-  program = get_program(instructionpart)
+  program = get_program(program_input)
+  
+  # part one
+  stacks = get_stacks(crate_input)
+  stacks_result = rearrange(stacks, program, True)
 
-  for i in program:
-    q = i.get('q')
-    o = i.get('from') - 1
-    d = i.get('to') - 1
-    take = stacks[o][-q:]
-    take.reverse()
-    stacks[o] = stacks[o][:-q]
-    stacks[d].extend(take)
-
-  result = ''
-  for i in range(len(stacks)):
-    result += stacks[i][-1:][0]
-  print(result)
+  print(calc_result(stacks_result))
 
   # part two
-  stacks = get_stacks(cratepart)
+  stacks = get_stacks(crate_input)
+  stacks_result = rearrange(stacks, program, False)
 
-  for i in program:
-    q = i.get('q')
-    o = i.get('from') - 1
-    d = i.get('to') - 1
-    take = stacks[o][-q:]
-    stacks[o] = stacks[o][:-q]
-    stacks[d].extend(take)
-
-  result = ''
-  for i in range(len(stacks)):
-    result += stacks[i][-1:][0]
-  print(result)
-  
+  print(calc_result(stacks_result))  
