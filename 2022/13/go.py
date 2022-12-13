10cc18293895d91b991348e36d2b397463e8057d
Ryszard Chrystyniak <okazet@gmail.com>
2022-12-13 17:09:58 +0100

    day 13 refactor

--- 2022/13/go.py
+++ 2022/13/go.py
@@ -1,59 +1,43 @@
-import ast
+from ast import literal_eval
 from functools import cmp_to_key
 
-def compare(left, right):
+def compare(a, b):
+  a = literal_eval(str(a))
+  b = literal_eval(str(b))
 
-  if isinstance(left, int) and isinstance(right, int):
-    if left < right: return True
-    if left > right: return False
-    return None
+  if type(a) is int and type(b) is int: 
+    return a - b
 
-  if isinstance(left, list) and isinstance(right, list):
-    size = min(len(left), len(right))
-    
-    for i in range(size):  
-      result = compare(left[i], right[i])
-      if result != None: return result
-    
-    if len(left) < len(right): return True
-    if len(left) > len(right): return False
-    return None
+  if type(a) is list and type(b) is list:
+    result = [compare(za, zb) for za, zb in zip(a, b)]
+    return next((r for r in result if r), len(a) - len(b))    
   
-  if isinstance(left, int) and isinstance(right, list):
-    if right: return compare([left], right)
-    elif not right: return compare([left], [0])
+  if type(a) is int and type(b) is list:
+    return compare([a], b if b else [0])
     
-  if isinstance(left, list) and isinstance(right, int):
-    if left: return compare(left, [right])
-    elif not left: return compare([0], [right])
+  if type(a) is list and type(b) is int:
+    return compare(a if a else [0], [b])
   
-  return False
+  return 0
 
 with open('input.txt') as f:
-  file = f.read()
-  pairs = file.split('\n\n')
-  pairs = [pair.split('\n') for pair in pairs]
+  signals = list(filter(len, f.read().splitlines()))
+ 
+  result = 0
 
-  result = []
-  for i, (left, right) in enumerate(pairs):
-    index = i + 1
-    l = ast.literal_eval(left)
-    r = ast.literal_eval(right)
+  for i in range(len(signals) // 2):
+    a, b = signals[i*2:i*2+2]
+    if compare(a, b) < 0: result += (i + 1)
+      
+  print('part 1:', result)
 
-    comparison = compare(l, r)
-    if comparison: result.append(index)
+  dividers = ['[[2]]', '[[6]]']
+  signals.extend(dividers)
+  asc = sorted(signals, key=cmp_to_key(compare))
 
-  print('part 1:', sum(result))
+  result = 1
 
-  signals = [line for line in filter(lambda l: l, file.splitlines())]
-  signals.extend(['[[2]]', '[[6]]'])
+  for i, signal in enumerate(asc):
+    if signal in dividers: result *= i + 1
 
-  def compareList(a, b):
-    a = ast.literal_eval(a)
-    b = ast.literal_eval(b)
-
-    return -1 if compare(a, b) else 1
-
-  in_order = sorted(signals, key=cmp_to_key(compareList))
-
-  print('part 2', (in_order.index('[[2]]') + 1) * (in_order.index('[[6]]') + 1))
+  print('part 2', result)
